31d26b47f73299bf4699a4b61a9b25fa55b63f52
2sh <git@2sh.me>
2023-05-29 02:33:28 +0100

    Licensing stuff

--- num-radix.ts
+++ num-radix.ts
@@ -1,6 +1,15 @@
+/*
+ * num-radix
+ * Copyright (C) 2023 2sh <contact@2sh.me>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 import Decimal from 'decimal.js'
 
-// Using options from:
+// Basing options on:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat
 
 type Options = {
